60abdbff23f185ba5c947667c108ec92930b2191
Juri Wolf <Amerlander@users.noreply.github.com>
2024-10-05 14:26:08 +0200

    Deploy site

--- _symbols/32f59346-d2df-4ad1-957e-8935287fb9e4.js
+++ _symbols/32f59346-d2df-4ad1-957e-8935287fb9e4.js
@@ -1,4 +1,4 @@
-// Featured People - Updated September 30, 2024
+// Featured People - Updated October 5, 2024
 function noop() { }
 function assign(tar, src) {
     // @ts-ignore
--- _symbols/8cb1d0c1-0e2a-47ee-923a-c96ed27a995b.js
+++ _symbols/8cb1d0c1-0e2a-47ee-923a-c96ed27a995b.js
@@ -1,4 +1,4 @@
-// Box - Updated September 30, 2024
+// Box - Updated October 5, 2024
 function noop() { }
 function assign(tar, src) {
     // @ts-ignore
